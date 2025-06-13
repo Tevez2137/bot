6e3fb6d450eeae4388181d6d5a34e22d1d46b84c
Tevez2137 <karolgorlik1@gmail.com>
2025-06-13 17:53:52 +0200

    1

--- db/database.py
+++
@@ -1,8 +0,0 @@
-def polacz(host, login, haslo, db):
-    db = mysql.connector.connect(
-            host=host,
-            user=login,
-            password=haslo,
-            database=db
-        )
-cursor = db.cursor()
--- main.py
+++ main.py
@@ -4,12 +4,18 @@ from discord import app_commands
 import mysql.connector
 from discord.ui import View, Select, Button
 from trader.trader import trader
-import db.database
+
 
 intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
 
 if __name__ == '__main__':
-    polacz("mysql://root:ixltAmoiTlhxCSfcllrbENEtUyawnhhS@mysql.railway.internal:3306/railway", "root", "ixltAmoiTlhxCSfcllrbENEtUyawnhhS", "bocik")
+    db = mysql.connector.connect(
+        host="mysql://root:ixltAmoiTlhxCSfcllrbENEtUyawnhhS@mysql.railway.internal:3306/railway",
+        user="root",
+        password="ixltAmoiTlhxCSfcllrbENEtUyawnhhS",
+        database="bocik"
+    )
+    cursor = db.cursor()
     trader()
