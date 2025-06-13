e32e927ca6336e137e63c2bf8aa60041bd985f11
Tevez2137 <karolgorlik1@gmail.com>
2025-06-13 17:52:21 +0200

    1

--- main.py
+++ main.py
@@ -4,10 +4,12 @@ from discord import app_commands
 import mysql.connector
 from discord.ui import View, Select, Button
 from trader.trader import trader
+import db.database
 
 intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
 
 if __name__ == '__main__':
+    polacz("mysql://root:ixltAmoiTlhxCSfcllrbENEtUyawnhhS@mysql.railway.internal:3306/railway", "root", "ixltAmoiTlhxCSfcllrbENEtUyawnhhS", "bocik")
     trader()
